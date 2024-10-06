ae42a374743b599d63470816661264f249e87034
Bareerah-zulquarnain <bareerah.zulquarnain@gmail.com>
2024-10-06 14:54:24 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+class student:
+    grade = 6
+    name = "Bareerah"
+
+
+    def introduction (self):
+        print("Hi iam a student ")
+
+    def deatils(self):
+        print("my name is", self.name)
+        print("i study in ", self.grade)
+        
+ob = student()
+ob.introduction() 
+ob.deatils()
+
